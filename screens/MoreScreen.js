55ee7d0516ee9b0196fa825f75cafa1cae5c7dd0
Muhammad Saad <muhammadsaad8697@gmail.com>
2025-02-18 12:25:00 +0000

    fix errors in MoreScreen
    fixed Logging out issues, state management, integrated Logging out loading screen

--- screens/MoreScreen.js
+++ screens/MoreScreen.js
@@ -1,7 +1,39 @@
-import React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  ActivityIndicator,
+} from "react-native";
+import { signOut } from "firebase/auth";
+import { auth } from "../config/firebaseConfig";
 
 const MoreScreen = ({ navigation }) => {
+  const [loggingOut, setLoggingOut] = useState(false);
+
+  const handleLogout = async () => {
+    setLoggingOut(true);
+    setTimeout(async () => {
+      try {
+        await signOut(auth);
+        console.log("User logged out!");
+      } catch (err) {
+        console.error(err);
+      } finally {
+        setLoggingOut(false);
+      }
+    }, 2000); // Simulate loading time of 2 seconds
+  };
+  if (loggingOut) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.logo}>ecoBites</Text>
+        <ActivityIndicator size="large" color="#42a5f5" />
+        <Text style={styles.loadingText}>Logging out...</Text>
+      </View>
+    );
+  }
   return (
     <View style={styles.container}>
       <Text style={styles.title}>More Options</Text>
@@ -10,11 +42,7 @@ const MoreScreen = ({ navigation }) => {
         onPress={() => navigation.navigate("About Us")}
       />
       <Button title="Profile" onPress={() => navigation.navigate("Profile")} />
-      <Button
-        title="Logout"
-        onPress={() => navigation.navigate("Logout")}
-        color="#d9534f"
-      />
+      <Button title="Logout" onPress={handleLogout} color="#d9534f" />
     </View>
   );
 };
@@ -25,6 +53,18 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     padding: 20,
+  },
+  logo: {
+    fontSize: 36,
+    color: "#42a5f5",
+    letterSpacing: 3,
+    marginBottom: 20,
+    fontFamily: "Courier",
+  },
+  loadingText: {
+    marginTop: 10,
+    fontSize: 16,
+    color: "#555",
   },
   title: {
     fontSize: 24,
